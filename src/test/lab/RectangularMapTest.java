5b5272678814d0bddb4edf4aa744ce5613cdd7e6
Piotrek <piotr.kica.35@gmail.com>
2020-10-22 22:07:23 +0200

    lab4 unit test

---
+++ src/test/lab/RectangularMapTest.java
@@ -0,0 +1,44 @@
+package lab;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RectangularMapTest {
+    private IWorldMap map = new RectangularMap(10, 5);
+
+    @Test
+    public void testCanMoveTo(){
+        assertFalse(map.canMoveTo(new Vector2d(-1,-1)));
+        assertFalse(map.canMoveTo(new Vector2d(11,2)));
+        assertFalse(map.canMoveTo(new Vector2d(2,6)));
+        assertTrue(map.canMoveTo(new Vector2d(2,2)));
+
+        map.place(new Animal(map, new Vector2d(4,4)));
+        assertFalse(map.canMoveTo(new Vector2d(4,4)));
+    }
+
+    @Test
+    public void testPlace(){
+        assertTrue(map.place(new Animal(map, new Vector2d(4,4))));
+        assertFalse(map.place(new Animal(map, new Vector2d(4,4))));
+    }
+
+    @Test
+    public void testIsOccupied(){
+        map.place(new Animal(map, new Vector2d(4,4)));
+        map.place(new Animal(map, new Vector2d(3,3)));
+        assertTrue(map.isOccupied(new Vector2d(3,3)));
+        assertFalse(map.isOccupied(new Vector2d(1,1)));
+    }
+
+    @Test
+    public void testObjectAt(){
+        map.place(new Animal(map, new Vector2d(4,4)));
+        Assert.assertNull(map.objectAt(new Vector2d(1,1)));
+        Animal animal = new Animal(map);
+        map.place(animal);
+        assertEquals(animal, map.objectAt(new Vector2d(2,2)));
+    }
+}
