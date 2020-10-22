c411454350d826c8269daabdba5d17942d28c8ea
Piotrek <piotr.kica.35@gmail.com>
2020-10-22 21:00:26 +0200

    lab4

---
+++ src/main/lab/RectangularMap.java
@@ -0,0 +1,66 @@
+package lab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RectangularMap implements IWorldMap {
+    private Vector2d botLeft;
+    private Vector2d topRight;
+    private List<Animal> animals = new ArrayList<>();
+    private MapVisualizer mapVis = new MapVisualizer(this);
+
+    public RectangularMap(int width, int height){
+        this.botLeft = new Vector2d(0,0);
+        this.topRight = new Vector2d(width, height);
+    }
+
+    public String toString(){
+        return mapVis.draw(botLeft, topRight);
+    }
+
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        if (isOccupied(position)){
+            return false;
+        }
+        return position.follows(botLeft) && position.precedes(topRight);
+    }
+
+    @Override
+    public boolean place(Animal animal) {
+        if (this.isOccupied(animal.getPosition())){
+            return false;
+        }
+        animals.add(animal);
+        return true;
+    }
+
+    @Override
+    public void run(MoveDirection[] directions) {
+        for(int i = 0; i < directions.length;i++){
+            Animal movedAnimal = animals.get(i % animals.size());
+            movedAnimal.move(directions[i]);
+            animals.set(i % animals.size(), movedAnimal);
+        }
+    }
+
+    @Override
+    public boolean isOccupied(Vector2d position) {
+        for (Animal animal: animals){
+            if (animal.getPosition().equals(position)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Object objectAt(Vector2d position) {
+        for (Animal animal: animals){
+            if (animal.getPosition().equals(position)){
+                return animal;
+            }
+        }
+        return null;
+    }
+}
