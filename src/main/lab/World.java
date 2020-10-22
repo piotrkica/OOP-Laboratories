29ab3a09e9dbb55ac6961b1be852a3f471517d7d
Piotrek <piotr.kica.35@gmail.com>
2020-10-22 23:05:40 +0200

    lab4 edit

--- src/main/lab/World.java
+++ src/main/lab/World.java
@@ -3,8 +3,8 @@ package lab;
 public class World {
 
     public static void main(String[] args) {
-        String[] arg = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
-        MoveDirection[] directions = new OptionsParser().parse(arg);
+        String[] dirs = {"f", "b", "r", "l", "f", "f"};
+        MoveDirection[] directions = new OptionsParser().parse(dirs);
         IWorldMap map = new RectangularMap(10, 5);
         map.place(new Animal(map));
         map.place(new Animal(map,new Vector2d(3,4)));
