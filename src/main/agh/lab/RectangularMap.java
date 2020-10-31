f951fdf4d73128ef9d98ad3353a4af2aa114abdc
Piotrek <piotr.kica.35@gmail.com>
2020-10-31 13:39:41 +0100

    undo width -1, height -1

--- src/main/agh/lab/RectangularMap.java
+++ src/main/agh/lab/RectangularMap.java
@@ -10,7 +10,7 @@ public class RectangularMap extends AbstractWorldMap {
 
     public RectangularMap(int width, int height) {
         this.bottomLeft = new Vector2d(0, 0);
-        this.topRight = new Vector2d(width - 1, height - 1);
+        this.topRight = new Vector2d(width, height);
     }
 
     public Vector2d[] getMapBoundaries() {
