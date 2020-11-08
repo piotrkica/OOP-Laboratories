427a733d6a864469aba05ebf83bc4f1bdb84f693
Piotrek <piotr.kica.35@gmail.com>
2020-11-08 20:56:17 +0100

    lab6, using SimulationEngine

--- src/test/agh/lab/IntegrationTests.java
+++ src/test/agh/lab/IntegrationTests.java
@@ -1,37 +1,42 @@
 package agh.lab;
 
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Map;
 import static org.junit.Assert.*;
 
 public class IntegrationTests {
-    private final IWorldMap map = new RectangularMap(10, 5);
+    private final RectangularMap rectMap = new RectangularMap(10, 5);
     private final OptionsParser parser = new OptionsParser();
-    private final IWorldMap grassMap = new GrassField(10);
+    private final GrassField grassMap = new GrassField(10);
 
     @Test
-    public void testIntegrationRectangular(){
-        Animal wombat = new Animal(map);
-        Animal lemur = new Animal(map, new Vector2d(3,4));
-        map.place(wombat);
-        map.place(lemur);
-
-        String [] directions = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
-        map.run(parser.parse(directions));
-        assertEquals(new Vector2d(2,0), wombat.getPosition());
-        assertEquals(new Vector2d(3,5), lemur.getPosition());
+    public void testIntegrationRectangular() {
+        Vector2d[] startingPositions = {new Vector2d(2,2), new Vector2d(3,4)};
+        String[] directions = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
+        SimulationEngine engine = new SimulationEngine(parser.parse(directions), rectMap, startingPositions);
+        engine.run();
+        Map<Vector2d, Animal> animalsHM = rectMap.getAnimalsHM();
+        Iterator<Map.Entry<Vector2d, Animal>> animals = animalsHM.entrySet().iterator();
+        Map.Entry<Vector2d, Animal> wombat = animals.next();
+        Map.Entry<Vector2d, Animal> lemur = animals.next();
+        assertEquals(new Vector2d(2,0), wombat.getKey());
+        assertEquals(new Vector2d(3,5), lemur.getKey());
     }
 
     @Test
-    public void testIntegrationGrassField(){
-        Animal wombat = new Animal(grassMap);
-        Animal lemur = new Animal(grassMap, new Vector2d(3,4));
-        grassMap.place(wombat);
-        grassMap.place(lemur);
-
-        String [] directions = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
-        grassMap.run(parser.parse(directions));
-        assertEquals(new Vector2d(2,-1), wombat.getPosition());
-        assertEquals(new Vector2d(3,7), lemur.getPosition());
+    public void testIntegrationGrassField() {
+        Vector2d[] startingPositions = {new Vector2d(2,2), new Vector2d(3,4)};
+        String[] directions = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
+        SimulationEngine engine = new SimulationEngine(parser.parse(directions), grassMap, startingPositions);
+        engine.run();
+        Map<Vector2d, Animal> animalsHM = grassMap.getAnimalsHM();
+        Iterator<Map.Entry<Vector2d, Animal>> animals = animalsHM.entrySet().iterator();
+        Map.Entry<Vector2d, Animal> wombat = animals.next();
+        Map.Entry<Vector2d, Animal> lemur = animals.next();
+        assertEquals(new Vector2d(3,7), wombat.getKey());
+        assertEquals(new Vector2d(2,-1), lemur.getKey());
     }
 
 }
