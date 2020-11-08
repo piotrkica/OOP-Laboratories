9b7a97aec48ea0f12ef27b964641eb51325aaa5d
Piotrek <piotr.kica.35@gmail.com>
2020-11-08 20:28:46 +0100

    final position

--- src/main/agh/lab/Grass.java
+++ src/main/agh/lab/Grass.java
@@ -1,7 +1,7 @@
 package agh.lab;
 
 public class Grass implements IMapElement {
-    private Vector2d position;
+    private final Vector2d position;
 
     public Grass(Vector2d position){
         this.position = position;
